80b205aaf7c0f40b9663e905fa2dbc55daf34f67
Alvaro Sanchez-Mariscal <alvaro.sanchez@odobo.com>
2014-03-12 17:06:13 +0100

    Added Spock and refactored packages

---
+++ src/test/groovy/com/odobo/filestorage/rackspace/RackspaceStorageProviderSpec.groovy
@@ -0,0 +1,14 @@
+package com.odobo.filestorage.rackspace
+
+import spock.lang.Specification
+
+/**
+ * TODO: write doc
+ */
+class RackspaceStorageProviderSpec extends Specification {
+
+    def "foo"() {
+        expect:
+        true
+    }
+}
